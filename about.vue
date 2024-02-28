b0df00f4aa75f1bae9faf545d4e336da6e96a7c3
TanasiriChar <160839002+TanasiriChar@users.noreply.github.com>
2024-02-28 23:14:30 +0700

    Add files via upload

---
+++ about.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <header>
+        <nav>
+          <h1 class="logo">Tanasiri K</h1>
+          <NuxtLink to="/">หน้าเเรกของเว็บ</NuxtLink>
+          <NuxtLink to="/about">เกี่ยวกับเรา</NuxtLink>
+          <NuxtLink to="/products">รายละเอียดข้อมูล</NuxtLink>
+          <NuxtLink to="/products/detail">รายละเอียดข้อมูลเว็บ</NuxtLink>
+        </nav>
+     </header>
+    <div>
+        <h1>เกี่ยวกับเรา</h1>
+    </div>   
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style coped>
+nav{
+  margin: 10px auto;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background:#333;
+}
+nav a{
+  color: white;
+  text-decoration: none;
+  margin-left: 12px;
+  font-size: 18px;
+  padding: 10px;
+}
+nav a:hover{
+  color: orange;
+}
+.logo{
+  margin-right: auto;
+  padding-left: 10px;
+  color: white;
+}
+</style>
---
+++ index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>หน้าเเรก</h1>
+    <img src="/image/koo1.png">
+  
+  </div>
+</template>
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
